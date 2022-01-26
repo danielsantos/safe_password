5c3a76d0da2dea662b17fc065d2abd54c6de1795
Daniel Rocha <daniel.rocha@riocardmais.com.br>
2022-01-26 11:41:30 -0300

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:safe_password/pages/create_password.dart';
 import 'package:safe_password/pages/edit_password.dart';
 import 'package:safe_password/pages/home.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const HomePage(),
+      home: MyHomePage(title: 'Splash Screen Flutter'),
       routes: {
         '/home': (ctx) => const HomePage(),
         '/create': (ctx) => CreatePasswordPage(),
@@ -23,4 +24,55 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 3),
+    vsync: this,
+  )..repeat(reverse: true);
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: _animation,
+      child: _introScreen(),
+    );
+  }
+}
+
+Widget _introScreen() {
+  return Stack(
+    children: <Widget>[
+      SplashScreen(
+        seconds: 4,
+        backgroundColor: Colors.white,
+        navigateAfterSeconds: const HomePage(),
+        loaderColor: Colors.transparent,
+      ),
+      Center(
+        child: Container(
+          child: Image.asset('assets/images/icon.png'),
+        ),
+      ),
+    ],
+  );
 }
